1c02a3727b359e39620c82d2a93b7128fcb6d1a4
Meet Patel <meetaiims@yahoo.com>
2022-11-24 18:30:55 -0500

    plot theoretical/expected probability distribution for fair D6 normalized to 1

--- die.py
+++ die.py
@@ -6,15 +6,21 @@ class Die:
     def __init__(self, num_sides=6):
         """Assume a six-sided die."""
         self.num_sides = num_sides
+        self.outcomes = range(1, num_sides+1)
 
-
-    def fair_die_dist(self):
+    def set_fair_die_dist(self):
         uniform_prob = 1/self.num_sides
         prob_dict = {}
-        for i in range(1, self.num_sides+1):
+        for i in self.outcomes:
             prob_dict[i] = uniform_prob
-        return prob_dict
+        self.fair_die_dist =  prob_dict
 
-    def roll(self):
+    def roll_once(self):
         """Return a random value between 1 and number of sides."""
         return randint(1, self.num_sides)
+
+    def roll_n(self, n):
+        """Roll Die n times"""
+        return choices(self.outcomes, k=n)
+
+
