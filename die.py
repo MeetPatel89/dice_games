350d9bc9d052ecde80afdb7772d8e303b6d4fa65
Meet Patel <meetaiims@yahoo.com>
2022-11-24 12:35:01 -0500

    get probability distribution for fair D6

--- die.py
+++ die.py
@@ -7,6 +7,13 @@ class Die:
         """Assume a six-sided die."""
         self.num_sides = num_sides
 
+    def fair_die_dist(self):
+        uniform_prob = 1/self.num_sides
+        prob_dict = {}
+        for i in range(1, self.num_sides+1):
+            prob_dict[i] = uniform_prob
+        return prob_dict
+
     def roll(self):
         """Return a random value between 1 and number of sides."""
         return randint(1, self.num_sides)
