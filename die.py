cf191b50fe1db2c536cbc61dd53d7cce7400cc9e
Meet Patel <meetaiims@yahoo.com>
2022-11-24 13:16:02 -0500

    use choices method of random module to roll D6 1000 times

--- die.py
+++ die.py
@@ -1,4 +1,4 @@
-from random import randint 
+from random import randint, choices
 
 class Die:
     """ A class representing a single die object."""
@@ -6,6 +6,7 @@ class Die:
     def __init__(self, num_sides=6):
         """Assume a six-sided die."""
         self.num_sides = num_sides
+
 
     def fair_die_dist(self):
         uniform_prob = 1/self.num_sides
